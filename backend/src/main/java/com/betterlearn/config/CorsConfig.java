6c0defd80992bda513118490e1ff55950bea4ec3
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2026-03-11 10:31:17 -0400

    update CORS to point to betterlearn.app

--- backend/src/main/java/com/betterlearn/config/CorsConfig.java
+++ backend/src/main/java/com/betterlearn/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     CorsFilter corsFilter() {
         var config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://betterlearn.app", "http://betterlearn.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
